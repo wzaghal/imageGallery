731e8b854b54436108f3704e406109e52b6cf2c5
wzaghal <wisamzaghal@gmail.com>
2014-05-17 21:04:14 -0400

    Add music js file

---
+++ music.js
@@ -0,0 +1,29 @@
+var music = [
+	{
+		'id':1,
+		'artist':'Beatles',
+		'imageUrl':'images/albums/album-abbeyroad-large.jpg',
+		'Description':'lorem'
+	},
+	{
+		'id':2,
+		'artist':'Bob Marley',
+		'imageUrl':'images/albums/album-bobmarley-large.jpg',
+		'Description':'lorem'
+	},
+	{
+		'artist':'Pink Floyd',
+		'imageUrl':'images/albums/album-pinkfloyd-large.jpg',
+		'Description':'lorem'
+	},
+	{
+		'artist':'Jay-Z',
+		'imageUrl':'images/albums/album-yeezus-large.jpg',
+		'Description':'lorem'
+	},
+	{
+		'artist':'Kanye West',
+		'imageUrl':'images/albums/album-yeezus-large.jpg',
+		'Description':'lorem'
+	}
+]
