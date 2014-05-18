4c8ccad1d2977b612a641e759ca9f1911a889775
wzaghal <wisamzaghal@gmail.com>
2014-05-17 23:27:47 -0400

    more JS functionality and less hard-coding

--- music.js
+++ music.js
@@ -2,6 +2,7 @@ var music = [
 	{
 		'id':1,
 		'artist':'Beatles',
+		'album': 'albumName',
 		'imageUrlLarge':'images/albums/album-abbeyroad-large.jpg',
 		'imageUrlSmall':'images/albums/album-abbeyroad-small.jpg',
 		'Description':'lorem'
@@ -9,23 +10,31 @@ var music = [
 	{
 		'id':2,
 		'artist':'Bob Marley',
+		'album': 'albumName',
 		'imageUrlLarge':'images/albums/album-bobmarley-large.jpg',
-		'imageUrlSmall':'images/albums/album-bobmarley-large.jpg',
+		'imageUrlSmall':'images/albums/album-bobmarley-small.jpg',
 		'Description':'lorem'
 	},
 	{	'id':3,
-		'artist':'Pink Floyd',
-		'imageUrlLarge':'images/albums/album-pinkfloyd-large.jpg',
+		'artist':'Kanye West',
+		'album': 'albumName',
+		'imageUrlLarge':'images/albums/album-yeezus-large.jpg',
+		'imageUrlSmall':'images/albums/album-yeezus-small.jpg',
 		'Description':'lorem'
 	},
 	{	'id':4,
-		'artist':'Jay-Z',
-		'imageUrl':'images/albums/album-yeezus-large.jpg',
-		'Description':'lorem'
+		'artist':'Pink Floyd',
+		'album': 'albumName',
+		'imageUrlLarge':'images/albums/album-pinkfloyd-large.jpg',
+		'imageUrlSmall':'images/albums/album-pinkfloyd-small.jpg',
+		'Description':'lorem',
 	},
 	{	'id':5,
-		'artist':'Kanye West',
-		'imageUrl':'images/albums/album-yeezus-large.jpg',
-		'Description':'lorem'
+		'artist':'Jay-Z',
+		'album': 'albumName',
+		'imageUrlLarge':'images/albums/album-magnacarta-large.jpg',
+		'imageUrlSmall':'images/albums/album-magnacarta-small.jpg',
+		'Description':'lorem',
 	}
+
 ]
